7cee613266f049977c3625795b9b839f5243f366
bartolstelcer <133920359+bartolstelcer@users.noreply.github.com>
2023-06-01 13:35:45 +0200

    Update igrac.h

--- igrac.h
+++ igrac.h
@@ -1,3 +1,5 @@
+#pragma once
+
 //6
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -7,18 +9,17 @@
 
 //3
 
-typedef struct {
-    char ime[50];
-    char nadimak[50];
-    char prezime[50];
-    char lozinka[50];
-} Igrac;
+typedef struct igrac {
+	char ime[50];
+	char nadimak[50];
+	char prezime[50];
+} IGRAC;
 
 //4
 
-void savePlayerInfo(const Igrac* player);
-Igrac* loadPlayerInfo();
-void playGame(Igrac* player);
-int checkPassword(const Igrac* player, const char* password);
+void savePlayerInfo(const IGRAC* player);
+void playGame(IGRAC* player);
+int checkPassword(const IGRAC* player, const char* password);
 
 #endif  // IGRAC_H
+
