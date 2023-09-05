5ca0bfb8462d5582b0b6c514eda4f734e9780f1a
bartolstelcer <133920359+bartolstelcer@users.noreply.github.com>
2023-09-05 05:57:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-//6
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
@@ -7,264 +5,192 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
-#include "igrac.h"
-#include "ljestvica.h"
+
+#define MULTIPLY_SCORE(bodoviPrije, multiplikator) (bodoviPrije * multiplikator)
+
+typedef struct {
+    char ime[50];
+    char prezime[50];
+    char nadimak[50];
+} Igrac;
+
+void unesiPodatkeIgraca(Igrac* igrac) {
+    printf("Unesite ime: ");
+    scanf("%s", igrac->ime);
+    printf("Unesite prezime: ");
+    scanf("%s", igrac->prezime);
+    printf("Unesite nadimak: ");
+    scanf("%s", igrac->nadimak);
+}
+
+void unesiBodoveUDatoteku(Igrac igrac, int tezinaIgre, int bodoviPoslije) {
+    FILE* file;
+    file = fopen("ljestvica.txt", "a");
+
+    if (file != NULL) {
+        fprintf(file, "%s - %2s - %2s - %2d - %2d\n", igrac.ime, igrac.nadimak, igrac.prezime, tezinaIgre, bodoviPoslije);
+        fclose(file);
+    }
+    else {
+        printf("Nije moguce otvoriti datoteku za pisanje.\n");
+    }
+}
+
+void unesiPodatkeIgracaUDatoteku(Igrac igrac) {
+    FILE* file;
+    file = fopen("igrac.txt", "w");
+
+    if (file != NULL) {
+        fprintf(file, "Ime: %s\nPrezime: %s\nNadimak: %s\n", igrac.ime, igrac.prezime, igrac.nadimak);
+        fclose(file);
+    }
+    else {
+        printf("Nije moguce otvoriti datoteku za pisanje.\n");
+    }
+}
+
+void prikaziLjestvicu() {
+    FILE* file;
+    char linija[200];
+
+    file = fopen("ljestvica.txt", "r");
+
+    if (file != NULL) {
+        printf("\nLjestvica:\n");
+
+        while (fgets(linija, sizeof(linija), file)) {
+            printf("%s", linija);
+        }
+
+        fclose(file);
+    }
+    else {
+        printf("Nema dostupnih rezultata u ljestvici.\n");
+    }
+}
+
+void igrajIgru(int tezinaIgre, Igrac igrac) {
+    int bodoviPrije = 0, bodoviPoslije = 0, brojKrugova = 0, maxBodovi = 0;
+    int multiplikator;
+
+    switch (tezinaIgre) {
+    case 1:
+        maxBodovi = 10;
+        multiplikator = 1;
+        break;
+    case 2:
+        maxBodovi = 7;
+        multiplikator = 2;
+        break;
+    case 3:
+        maxBodovi = 5;
+        multiplikator = 5;
+        break;
+    case 4:
+        maxBodovi = 3;
+        multiplikator = 10;
+        break;
+    default:
+        printf("Nevazeca tezina.\n");
+        return;
+    }
+
+    while (1) {
+        int krugBodovi = rand() % (maxBodovi + 1);
+
+        bodoviPrije = bodoviPrije + krugBodovi;
+        brojKrugova++;
+
+        printf("\nU %d. krugu ste osvojili %d ECTS bodova!\n", brojKrugova, krugBodovi);
+
+        if (krugBodovi == 0) {
+            bodoviPoslije = MULTIPLY_SCORE(bodoviPrije, multiplikator);
+            printf("Ukupno ste osvojili %d ECTS bodova prije mnozenja!\n", bodoviPrije);
+            printf("Ukupno ste osvojili %d ECTS bodova poslije mnozenja!\n", bodoviPoslije);
+            break;
+        }
+
+        printf("Zelite li nastaviti s igrom? (1/0): ");
+        int odgovor;
+        scanf("%d", &odgovor);
+
+        if (odgovor == 0) {
+            bodoviPoslije = MULTIPLY_SCORE(bodoviPrije, multiplikator);
+            printf("Ukupno ste osvojili %d ECTS bodova prije mnozenja!\n", bodoviPrije);
+            printf("Ukupno ste osvojili %d ECTS bodova poslije mnozenja!\n", bodoviPoslije);
+            break;
+        }
+    }
+
+    unesiBodoveUDatoteku(igrac, tezinaIgre, bodoviPoslije);
+}
 
 int main(void) {
+    srand(time(NULL));
 
-	//1
-	//2
+    Igrac igrac;
 
-	int izbor, tezinaIgreIzbor, brisanjeIzbor, pretrazivanjeIzbor, sortiranjeIzbor;
-	char povratakTezinaIgre, povratakBrisanje, povratakPretrazivanje, povratakSortiranje;
-	char izlazakIzPrograma;
+    unesiPodatkeIgraca(&igrac);
+    unesiPodatkeIgracaUDatoteku(igrac);
 
-	//5
+    while (1) {
+        printf("\nIzbornik:\n");
+        printf("1. Zapocni igru\n");
+        printf("2. Ljestvica\n");
+        printf("3. Brisanje datoteka\n");
+        printf("4. Preimenovanje datoteka\n");
+        printf("5. Izlazak iz igre\n");
+        printf("Odaberite opciju: ");
 
-	static IGRAC* currentPlayer = NULL;
+        int izbor;
+        scanf("%d", &izbor);
 
-	//8
+        switch (izbor) {
+        case 1: {
+            printf("\nIzbornik:\n");
+            printf("1. Easy mode\n");
+            printf("2. Medium mode\n");
+            printf("3. Hard mode\n");
+            printf("4. Impossible mode\n");
+            printf("Odaberite opciju: ");
 
-	do {
-		printf("Izbornik:\n");
-		printf("1. Zapocni igru\n");
-		printf("2. Rang ljestvica\n");
-		printf("3. Brisanje igraca na ljestvici\n");
-		printf("4. Pretrazivanje igraca na ljestvici\n");
-		printf("5. Sortiranje igraca na ljestvici\n");
-		printf("6. Pravila igre\n");
-		printf("7. Izlazak iz igre\n");
-		printf("Unesite vas izbor: ");
-		scanf("%d", &izbor);
-		getchar();
+            int tezina;
+            scanf("%d", &tezina);
 
-		switch (izbor) {
-		case 1:
-			if (currentPlayer != NULL) {
-				//14
-				free(currentPlayer);
-				currentPlayer = NULL;
-			}
+            igrajIgru(tezina, igrac);
+            break;
+        }
+        case 2:
+            prikaziLjestvicu();
+            break;
+        case 3:
+            if (remove("igrac.txt") == 0) {
+                printf("\nDatoteka 'igrac.txt' uspjesno obrisana.\n");
+            }
+            else {
+                printf("\nNije moguce obrisati datoteku 'igrac.txt'.\n");
+            }
+            break;
+        case 4:
+            if (rename("igrac.txt", "prijasnjiIgrac.txt") == 0) {
+                printf("\nDatoteka 'igrac.txt' uspjesno preimenovana.\n");
+            }
+            else {
+                printf("\nNije moguce preimenovati datoteku 'igrac.txt'.\n");
+            }
+            break;
+        case 5:
+            printf("\nZelite li izaci iz igre? (1/0): ");
+            int odgovor;
+            scanf("%d", &odgovor);
+            if (odgovor == 1) {
+                return 0;
+            }
+            break;
+        default:
+            printf("Nevazeci izbor!\n");
+        }
+    }
 
-			//14
-
-			currentPlayer = (IGRAC*)malloc(sizeof(IGRAC));
-			if (currentPlayer == NULL) {
-				printf("Dodjela memorije nije uspjela!\n");
-				break;
-			}
-
-			printf("Ime: ");
-			scanf("%s", currentPlayer->ime);
-			printf("Prezime: ");
-			scanf("%s", currentPlayer->prezime);
-			printf("Nadimak: ");
-			scanf("%s", currentPlayer->nadimak);
-
-			savePlayerInfo(currentPlayer);
-
-			//8
-
-			printf("Izaberite tezinu igre:\n");
-			printf("1. Easy mode\n");
-			printf("2. Medium mode\n");
-			printf("3. Hard mode\n");
-			printf("4. Impossible mode\n");
-			printf("5. Povratak\n");
-			printf("Unesite vas izbor: ");
-			scanf("%d", &tezinaIgreIzbor);
-			getchar();
-
-			switch (tezinaIgreIzbor) {
-			case 1:
-				printf("Pocetak igre u easy modu. . .\n");
-				break;
-			case 2:
-				printf("Pocetak igre u medium modu. . .\n");
-				break;
-			case 3:
-				printf("Pocetak igre u hard modu. . .\n");
-				break;
-			case 4:
-				printf("Pocetak igre u impossible modu. . .\n");
-				break;
-			case 5:
-				printf("Zelite li se vratiti na prijasnji izbornik? (da/ne): ");
-				scanf("%c", &povratakTezinaIgre);
-				break;
-			default:
-				printf("Nevazeca opcija!\n");
-				break;
-			}
-
-			playGame(currentPlayer);
-			break;
-
-		case 2:
-			displayLeaderboard();
-			break;
-
-		case 3:
-			printf("1. Brisanje igraca po imenu\n");
-			printf("2. Brisanje igraca po prezimenu\n");
-			printf("3. Brisanje igraca po nadimku\n");
-			printf("4. Brisanje igraca po broju osvojenih ECTS bodova\n");
-			printf("5. Brisanje igraca po nivou na kojem je igrac zavrsio igru\n");
-			printf("6. Povratak");
-			printf("Unesite vas izbor: ");
-			scanf("%d", &brisanjeIzbor);
-			
-			switch (brisanjeIzbor) {
-			case 1:
-				printf("Unesite ime igraca kojeg zelite obrisati: ");
-				scanf();
-				break;
-			case 2:
-				printf("Unesite prezime igraca kojeg zelite obrisati: ");
-				scanf();
-				break;
-			case 3:
-				printf("Unesite nadimak igraca kojeg zelite obrisati: ");
-				scanf();
-				break;
-			case 4:
-				printf("Unesite broj osvojenih ECTS bodova igraca kojeg zelite obrisati: ");
-				scanf();
-				break;
-			case 5:
-				printf("Unesite nivo na kojem je igrac zavrsio igru igraca kojeg zelite obrisati: ");
-				scanf();
-				break;
-			case 6:
-				printf("Zelite li se vratiti na prijasnji izbornik? (da/ne): ");
-				scanf("%c", &povratakBrisanje);
-				break;
-			default:
-				printf("Nevazeca opcija!\n");
-				break;
-			}
-			break;
-		
-		case 4:
-			printf("1. Pretrazivanje igraca po imenu\n");
-			printf("2. Pretrazivanje igraca po prezimenu\n");
-			printf("3. Pretrazivanje igraca po nadimku\n");
-			printf("4. Pretrazivanje igraca po broju osvojenih ECTS bodova\n");
-			printf("5. Pretrazivanje igraca po nivou na kojem je igrac zavrsio igru\n");
-			printf("6. Povratak");
-			printf("Unesite vas izbor: ");
-			scanf("%d", &pretrazivanjeIzbor);
-
-			switch (pretrazivanjeIzbor) {
-			case 1:
-				printf("Unesite ime igraca kojeg zelite pronaci: ");
-				scanf();
-				break;
-			case 2:
-				printf("Unesite prezime igraca kojeg zelite pronaci: ");
-				scanf();
-				break;
-			case 3:
-				printf("Unesite nadimak igraca kojeg zelite pronaci: ");
-				scanf();
-				break;
-			case 4:
-				printf("Unesite broj osvojenih ECTS bodova kojeg zelite pronaci: ");
-				scanf();
-				break;
-			case 5:
-				printf("Unesite nivo na kojem je igrac zavrsio igru igraca kojeg zelite pronaci: ");
-				scanf();
-				break;
-			case 6:
-				printf("Zelite li se vratiti na prijasnji izbornik? (da/ne): ");
-				scanf("%c", &povratakPretrazivanje);
-				break;
-			default:
-				printf("Nevazeca opcija!\n");
-				break;
-			}
-			break;
-		
-		case 5:
-				printf("1. Abecedno sortiranje igraca po imenu (A - Z)\n");
-				printf("2. Abecedno sortiranje igraca po imenu (Z - A)\n");
-				printf("3. Abecedno sortiranje igraca po prezimenu (A - Z)\n");
-				printf("4. Abecedno sortiranje igraca po prezimenu (Z - A)\n");
-				printf("5. Abecedno sortiranje igraca po nadimku (A - Z)\n");
-				printf("6. Abecedno sortiranje igraca po nadimku (Z - A)\n");
-				printf("7. Sortiranje igraca po najvise osvojenih ECTS bodova\n");
-				printf("8. Sortiranje igraca po najmanje osvojenih ECTS bodova\n");
-				printf("9. Sortiranje igraca po najvecem nivou na kojem je igrac zavrsio igru\n");
-				printf("10. Sortiranje igraca po najmanjem nivou na kojem je igrac zavrsio igru\n");
-				printf("11. Povratak");
-				printf("Unesite vas izbor: ");
-				scanf("%d", &sortiranjeIzbor);
-
-				switch (sortiranjeIzbor) {
-				case 1:
-					printf("Abecedno sortiranje igraca po imenu (A - Z): ");
-				case 2:
-					printf("Abecedno sortiranje igraca po imenu (Z - A): ");
-				case 3:
-					printf("Abecedno sortiranje igraca po prezimenu (A - Z): ");
-				case 4:
-					printf("Abecedno sortiranje igraca po prezimenu (Z - A): ");
-				case 5:
-					printf("Abecedno sortiranje igraca po nadimku (A - Z): ");
-				case 6:
-					printf("Abecedno sortiranje igraca po nadimku (Z - A): ");
-				case 7:
-					printf("Sortiranje igraca po najvise osvojenih ECTS bodova: ");
-				case 8:
-					printf("Sortiranje igraca po najmanje osvojenih ECTS bodova: ");
-				case 9:
-					printf("Sortiranje igraca po najvecem nivou na kojem je igrac zavrsio igru: ");
-				case 10:
-					printf("Sortiranje igraca po najmanjem nivou na kojem je igrac zavrsio igru: ");
-				case 11:
-					printf("Zelite li se vratiti na prijasnji izbornik? (da/ne): ");
-					scanf("%d", &povratakSortiranje);
-				default:
-					printf("Nevazeca opcija!\n");
-					break;
-				}
-			break;
-		case 6:
-			printf("Pravila igre: \n");
-			break;
-		
-		case 7:
-			printf("Zelite li izaci iz igre? (da/ne): ");
-			scanf("%c", &izlazakIzPrograma);
-
-			if (izlazakIzPrograma == 'DA' || izlazakIzPrograma == "da" || izlazakIzPrograma == "Da" || izlazakIzPrograma == 'dA') {
-				printf("Izlazak iz igre!\n");
-				if (currentPlayer != NULL) {
-
-					//14
-
-					free(currentPlayer);
-					currentPlayer = NULL;
-				}
-				return 0;
-			}
-			break;
-
-		default:
-			printf("Nevazeca opcija!\n");
-			break;
-		}
-
-		printf("\n");
-	} while (izbor = !7);
-
-	//15
-
-	if (currentPlayer != NULL) {
-		free(currentPlayer);
-		currentPlayer = NULL;
-	}
-
-	return 0;
+    return 0;
 }
